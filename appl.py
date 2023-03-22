0971610aba845ea48d166307bb52323f87a7ad44
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-03-21 21:07:21 -0400

    Added functionality to the program that puts the grabbed info to an excel file in the same folder as the code

--- appl.py
+++ appl.py
@@ -1,9 +1,15 @@
-import pyperclip #pyperclip is a clipboard module for python that allows you to manipulate clipboard data
-import re
+# This program grabs all phone numbers and emails from the users clipboard allowing them to quickly obtain them. Additionally the program writes the grabbed info and saves it to an excel workbook.
+
+import pyperclip # pyperclip is a clipboard module for python that allows you to manipulate clipboard data
+import re # import statement for regex
+from openpyxl import Workbook # imports the excel functionality to the program
 
 
 # Get clipboard content
 clipboard = pyperclip.paste()
+
+
+
 
 
 
@@ -17,7 +23,6 @@ phoneRegex = re.compile(r'''(
     (\s*(ext|x|ext.)\s*(\d{2,5}))? # extension
     )''', re.VERBOSE)
 
-
 #the regular expression pattern for emails
 emailRegex = re.compile(r'''(
     [a-zA-Z0-9._%+-]+ # username
@@ -28,10 +33,14 @@ emailRegex = re.compile(r'''(
 
 
 
+
+
+
+
 # Search for phone numbers and email addresses, if they exisst add them to each respective list
 phoneNumbers = []
 emailAddresses = []
-
+# append the data that matches the regexs in all the clipboard content to the respective lists
 for match in phoneRegex.findall(clipboard):
     phoneNumbers.append(match[0])
     
@@ -41,9 +50,55 @@ for match in emailRegex.findall(clipboard):
 
 
 
+
+
+
+
+
+wb = Workbook()  #creates a new excel workbook
+
+ws = wb.active # get the active excel work sheet
+
+
+
+
+
+
+
+# if a phone number or email was grabbed, write them to an excel sheet
+if phoneNumbers:
+    ws['A1'] = 'Phone Numbers' # title the A1 cell/column phone numbers
+    for i, phoneNumber in enumerate(phoneNumbers):
+        ws.cell(row = i + 2, column = 1).value = phoneNumber
+        
+        
+if emailAddresses:
+    ws['B1'] = 'Email Addresses' # title the B1 cell/column email addresses
+    for i, emailAddress in enumerate(emailAddresses):
+        ws.cell(row = i + 2, column = 2).value = emailAddress
+
+
+
+
+
+
+wb.save('grabbed_data.xlsx')  # saves the worksheet in the same directory/folder as my python code
+
+
+
+
+
+
+
+
 # copy extracted data to clipboard
 output = "Phone Numbers:\n" + "\n".join(phoneNumbers) + "\n\nEmail Addresses:\n" + "\n".join(emailAddresses)
 pyperclip.copy(output)
+
+
+
+
+
 
 
 
